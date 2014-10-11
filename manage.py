d6cc24f778363f884a5992f69f581f566cdcced1
Iain Hunter <iain.hunter@outlook.com>
2014-10-11 16:25:36 +0100

    Add flask bootstrap

--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from flask.ext.script import Manager
+
 from app import bootstrap_app
 
 app = bootstrap_app()  # Initialise the flask app.
 manager = Manager(app)
+
 
 
 manager = Manager(app)
