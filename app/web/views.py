20a552087fe3a65588ee91ad3ae35d7af73d0372
Heri Rusmanto <retphern@gmail.com>
2018-06-28 00:29:26 +0700

    WIP
    resources

--- app/web/views.py
+++ app/web/views.py
@@ -1,15 +1,26 @@
+import os
+
 from flask import render_template
 from . import main
 
 
+slide_dir = 'static/slides/'
+
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+
+    slide_files = [f for f in os.listdir(slide_dir)]
+    slide_files_number = len(slide_files)
+    return render_template("index.html", slide_files_number=slide_files_number, slide_files=slide_files)
 
 @main.route('/about')
 def about():
     return render_template('about.html')
 
+@main.route('/<filename>')
+def video(filename):
+    return render_template('presentation.html', title=filename, slide_file=filename)
 
-
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
